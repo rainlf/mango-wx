378c38e62b63572b4f9c8a868b04bb59b76dc992
heyu <heyu.zero@bytedance.com>
2026-04-27 16:05:35 +0800

    update

--- miniprogram/components/rank/rank.ts
+++ miniprogram/components/rank/rank.ts
@@ -8,9 +8,62 @@ Component({
 
   data: {
     isRefreshing: false,
+    rankMode: 'points',
+    displayList: [] as any[],
+  },
+
+  observers: {
+    listData() {
+      ;(this as any).updateDisplayList()
+    },
+  },
+
+  lifetimes: {
+    attached() {
+      ;(this as any).updateDisplayList()
+    },
   },
 
   methods: {
+    updateDisplayList() {
+      const list = Array.isArray((this as any).properties.listData) ? [...((this as any).properties.listData as any[])] : []
+      const rankMode = this.data.rankMode
+      const sortedList = list.sort((a, b) => {
+        if (rankMode === 'winRate') {
+          if ((b.winRate || 0) !== (a.winRate || 0)) {
+            return (b.winRate || 0) - (a.winRate || 0)
+          }
+          if ((b.totalGames || 0) !== (a.totalGames || 0)) {
+            return (b.totalGames || 0) - (a.totalGames || 0)
+          }
+        } else if ((b.points || 0) !== (a.points || 0)) {
+          return (b.points || 0) - (a.points || 0)
+        }
+        return (b.winCount || 0) - (a.winCount || 0)
+      }).map((item) => ({
+        ...item,
+        rankMetricText: rankMode === 'winRate'
+          ? `${(((item.winRate || 0) * 1000) / 10).toFixed(1)}%`
+          : String(item.points || 0),
+        rankMetricLabel: rankMode === 'winRate' ? '胜率' : '金币',
+        rankMetricPositive: rankMode === 'winRate' ? true : (item.points || 0) >= 0,
+      }))
+
+      this.setData({
+        displayList: sortedList,
+      })
+    },
+    switchRankMode(e: any) {
+      const mode = e.currentTarget.dataset.mode
+      if (!mode || mode === this.data.rankMode) {
+        return
+      }
+      this.setData({
+        rankMode: mode,
+      }, () => {
+        this.updateDisplayList()
+      })
+    },
     onRefresh() {
       if (this.data.isRefreshing) {
         return
